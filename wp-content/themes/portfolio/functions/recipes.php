8a8ccaeeffadf4b7bd8c48522ac65a05100fa15d
Jimmy <jimmy@jnjohnson.io>
2022-09-12 16:04:32 +0000

    First commit of cookbook-OCR branch. Added OCR conversion on page save, starting setting rules for categorizing raw text

--- wp-content/themes/portfolio/functions/recipes.php
+++ wp-content/themes/portfolio/functions/recipes.php
@@ -2,6 +2,7 @@
     class Recipes {
         function __construct() {
             add_action('init', array($this, 'init'));
+            add_action('acf/save_post', array($this, 'recipe_image_OCR'));
         }
     
         public function init() {
@@ -12,12 +13,12 @@
                     'singular_name' => 'Recipe'
                 ),
                 'public' => true,
-                'supports' => array('title', 'revisions'),
+                'supports' => array('title', 'editor', 'revisions'),
                 'has_archive' => true,
-                // 'rewrite' => array(
-                //     'slug' => 'events',
-                //     'with_front' => false
-                // )
+                'rewrite' => array(
+                    'slug' => 'recipes',
+                    'with_front' => false
+                )
             ));
     
             register_taxonomy('meals', 'recipes',
@@ -43,6 +44,27 @@
                     'show_admin_column' => true
                 )
             );
+        }
+        public function recipe_image_OCR($post_id) {
+            $saved_post = get_post($post_id);
+            if ($saved_post->post_type != 'recipes') {
+                return;
+            }
+            $img_url = get_fields($post_id)['recipe_image'];
+
+            // Checks if the image was updated when the post was saved
+            if (!empty($img_url)) {
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://freeocrapi.com/api',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array('file'=> new CURLFILE($img_url))));
+                $response = json_decode(curl_exec($curl), true);
+                curl_close($curl);
+                wp_update_post(['post_content' => strtolower($response['text'])]);
+            }
         }
     }
 
