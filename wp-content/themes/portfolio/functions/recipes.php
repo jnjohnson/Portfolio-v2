b09a80174b5de4b85d5a60c6159b645b15ce7525
Jimmy <jimmy@jnjohnson.io>
2022-09-15 03:28:19 +0000

    Removed attempts to parse recipe into description, ingredients and instructions. Now updates post title and slug on save

--- wp-content/themes/portfolio/functions/recipes.php
+++ wp-content/themes/portfolio/functions/recipes.php
@@ -2,57 +2,76 @@
     class Recipes {
         function __construct() {
             add_action('init', array($this, 'init'));
-            add_action('acf/save_post', array($this, 'recipe_image_OCR'));
+            add_action('acf/save_post', array($this, 'recipe_image_OCR'), 5);
         }
-    
+        
         public function init() {
             register_post_type('Recipes', array(
-                'label' => 'Recipes',
-                'labels' => array(
-                    'name' => 'Recipes',
+                'label'             => 'Recipes',
+                'labels'            => array(
+                    'name'          => 'Recipes',
                     'singular_name' => 'Recipe'
                 ),
-                'public' => true,
-                'supports' => array('title', 'editor', 'revisions'),
-                'has_archive' => true,
-                'rewrite' => array(
-                    'slug' => 'recipes',
-                    'with_front' => false
+                'public'            => true,
+                'supports'          => array('title', 'editor', 'revisions'),
+                'has_archive'       => true,
+                'rewrite'           => array(
+                    'slug'          => 'recipes',
+                    'with_front'    => false
                 )
             ));
-    
+            
+            register_taxonomy('source', 'recipes', 
+                array(
+                    'labels'            => array(
+                        'name'          => 'Source',
+                    ),
+                    'show_ui'           => true,
+                    'public'            => true,
+                    'hierarchical'      => true,
+                    'show_admin_column' => true
+                )
+            );
             register_taxonomy('meals', 'recipes',
                 array(
-                    'labels' => array(
-                        'name' => 'Meals',
+                    'labels'            => array(
+                        'name'          => 'Meals',
                         'singular_name' => 'Meal'
                     ),
-                    'show_ui' => true,
-                    'public' => true,
-                    'hierarchical' => true,
+                    'show_ui'           => true,
+                    'public'            => true,
+                    'hierarchical'      => true,
                     'show_admin_column' => true
                 )
             );
             register_taxonomy('cuisine', 'recipes',
                 array(
-                    'labels' => array(
-                        'name' => 'Cuisine'
+                    'labels'            => array(
+                        'name'          => 'Cuisine'
                     ),
-                    'show_ui' => true,
-                    'public' => true,
-                    'hierarchical' => true,
+                    'show_ui'           => true,
+                    'public'            => true,
+                    'hierarchical'      => true,
                     'show_admin_column' => true
                 )
             );
         }
+
+        // Detects if the image has been updated.
+        // If yes, use OCR API to get all of the text in the image
         public function recipe_image_OCR($post_id) {
             $saved_post = get_post($post_id);
             if ($saved_post->post_type != 'recipes') {
                 return;
             }
-            $img_url = get_fields($post_id)['recipe_image'];
 
             // Checks if the image was updated when the post was saved
+            $img_id = get_fields($post_id)['recipe_image'];
+            $new_img_id = $_POST['acf']['field_631e6695786ff'];
+            // if ($img_id == $new_img_id) return;
+
+            $img_url = wp_get_attachment_image_src($img_id, 'full')[0];
+            
             if (!empty($img_url)) {
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
@@ -63,66 +82,49 @@
                 CURLOPT_POSTFIELDS => array('file'=> new CURLFILE($img_url))));
                 $response = json_decode(curl_exec($curl), true);
                 curl_close($curl);
-                wp_update_post(['post_content' => strtolower($response['text'])]);
-                
                 $this->save_recipe_parts_to_acf_fields($response['text'], $post_id);
             }
         }
+
+        // Saves the recipe name, slug, page number, servings, recipe content, and total time to the post
         public function save_recipe_parts_to_acf_fields($recipe, $post_id) {
-            $description_end = $ingredients_end = 0;
-            $in_ingredients = $in_instructions = false;
-            $verbs = ['add', 'adjust', 'bring', 'remove', 'whisk'];
-            $recipe = get_the_content();
             $lines = preg_split("/\r\n|\n|\r/", $recipe);
+            array_pop($lines); // Removes the last line, which has an undefined character in it
             $line_count = sizeof($lines);
 
             // Gets the page number, which should be either the first or last index of the array.
             // Removing that index normalizes the array so future operations don't depend on which 
             // orientation the array is in.
+            // In the instance that the page number is in the last index, the OCR software puts the
+            // name of the section and the page number on the same line, with the page number being last
             if (intval($lines[0])) {
                 $page = array_shift($lines);
-            } else if (intval($lines[$line_count - 1])) {
-                $page = array_pop($lines);
+            } else {
+                $page_arr = explode(' ', array_pop($lines));
+                $page = $page_arr[sizeof($page_arr) - 1];
             }
-            $title = array_shift($lines);
-            
-            // Get the servings and total time
-            $split = explode('total time:', array_shift($lines));
-            $servings = $split[0];
-            $time = $split[1];
-            
-            foreach ($lines as $i => $line) {
-                $split = explode(' ', $line);
-
-                // Checks to see if the first word in the line is a number, which will mean the line is either 
-                // an ingredient or the first line of an instruction.
-                // Checks the second word in the line to see if it is among the list of verbs. 
-                // If it is, the line is the first line of an instruction.
-                // Obviously this is going to be prone to bugs, but it's the best idea I have right now 
-                // that doesn't use machine learning
-
-                if (intval($split[0]) && !array_search($split[1], $verbs)) {
-                    if (!$in_ingredients) {
-                        $in_ingredients = true;
-                        $description_end = $i;
-                    }
-                } else if (intval($split[0]) && array_search($split[1], $verbs)) {
-                    if (!$in_instructions) {
-                        $in_instructions = true;
-                        $ingredients_end = $i;
-                    }
+            foreach ($lines as $ln => $line) {
+                if (strpos($line, 'SERVES') !== false) {
+                    break;
                 }
             }
-            $description = implode(' ', array_slice($lines, 0, $description_end));
-            $ingredients = implode("\r\n", array_slice($lines, $description_end, $ingredients_end  - $description_end));
-            $instructions = implode("\r\n", array_slice($lines, $ingredients_end));
-            wp_update_post(['post_title' => $title]);
+            $title = ucwords(strtolower(implode(' ', array_slice($lines, 0, $ln))));
+            // Get the servings and total time
+            $split = explode('TOTAL TIME:', $lines[$ln]);
+            $servings = $split[0];
+            $time = $split[1];
+            wp_update_post([
+                'post_title'    => $title,
+                'post_content'  => $recipe,
+                'post_name'     => str_replace(' ', '-', strtolower($title))
+            ]);
             update_field('page', $page, $post_id);
             update_field('servings', $servings, $post_id);
             update_field('time', $time, $post_id);
-            update_field('description', $description, $post_id);
-            update_field('ingredients', $ingredients, $post_id);
-            update_field('instructions', $instructions, $post_id);
+
+            // There's too much variation in how recipes are formatted (even within a book)
+            // to be able to come up with rules to parse things out perfectly without using ML.
+            // Maybe I'll try to learn TensorFlow for the next part of this...
         }
     }
 
