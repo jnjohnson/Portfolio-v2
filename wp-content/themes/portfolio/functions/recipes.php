ac280cb4c14d421fd1ff290da1668fa2ae8a8f37
Jimmy <jimmy@jnjohnson.io>
2026-01-19 21:59:15 +0000

    updated with new OCR api

--- wp-content/themes/portfolio/functions/recipes.php
+++ wp-content/themes/portfolio/functions/recipes.php
@@ -61,12 +61,11 @@
         // Detects if the image has been updated.
         // If yes, use OCR API to get all of the text in the image
         public function recipe_image_OCR($post_id) {
-            $log = fopen("log.txt", "w") or die("unable to open file");
             $saved_post = get_post($post_id);
             if ($saved_post->post_type != 'recipes') {
                 return;
             } else if (get_field('editing', $post_id)) {
-                return;
+		        return;
             }
 
             // Checks if the image was updated when the post was saved
@@ -78,39 +77,34 @@
             
             if (!empty($img_url)) {
                 $curl = curl_init();
-
+                $params = ['image'=> curl_file_create($img_url, 'image/jpg')];
                 curl_setopt_array($curl, [
-                    CURLOPT_URL => "https://apis-freeocr-ai.p.rapidapi.com/extract?fields=%5B%22title%22%2C%20%22page%20number%22%2C%20%22servings%22%2C%22cook%20time%22%2C%22ingredients%22%5D&format=json",
+                    CURLOPT_URL => "https://apis-freeocr-ai.p.rapidapi.com/extract?fields=%5B%22title%22%2C%20%22page%20number%22%2C%20%22servings%22%2C%20%22cook%20time%22%2C%20%22ingredients%22%2C%20%22recipe%20steps%22%5D&format=json",
+                    CURLOPT_POST => true,
                     CURLOPT_RETURNTRANSFER => true,
                     CURLOPT_ENCODING => "",
                     CURLOPT_MAXREDIRS => 10,
                     CURLOPT_TIMEOUT => 30,
                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                     CURLOPT_CUSTOMREQUEST => "POST",
-                    CURLOPT_POSTFIELDS => "-----011000010111000001101001
-                        Content-Disposition: form-data; name='image'"
-                        .$img_url.
-                        "-----011000010111000001101001--",
+                    CURLOPT_POSTFIELDS => $params,
                     CURLOPT_HTTPHEADER => [
-                        "Content-Type: multipart/form-data; boundary=---011000010111000001101001",
+                        "Content-Type: multipart/form-data",
                         "x-rapidapi-host: apis-freeocr-ai.p.rapidapi.com",
-                        "x-rapidapi-key:" . get_option('FREE_OCR_API_KEY')
+                        "x-rapidapi-key:" . FREE_OCR_API_KEY
                     ],
                 ]);
-
-                $response = curl_exec($curl);
+            
+                $response = json_decode(curl_exec($curl), true);
                 $err = curl_error($curl);
-
+                
                 curl_close($curl);
-
+                
                 if ($err) {
-                    fwrite($log, "cURL Error #:" . $err);
                     echo "cURL Error #:" . $err;
                 } else {
-                    fwrite($log, "response: " . $response);
-                    echo $response;
+                    $this->save_recipe_parts_to_acf_fields($response, $post_id);
                 }
-                // $this->save_recipe_parts_to_acf_fields($response['text'], $post_id);
             }
         }
 
@@ -118,46 +112,40 @@
         // There's too much variation in how recipes are formatted (even within a book) to be able to 
         // come up with rules to parse things out perfectly without using ML.
         // Maybe I'll try to learn TensorFlow for the next part of this...
-        public function save_recipe_parts_to_acf_fields($recipe, $post_id) {
-            $lines = preg_split("/\r\n|\n|\r/", $recipe);
-            array_pop($lines); // Removes the last line, which has an undefined character in it
-            $line_count = sizeof($lines);
-
-            // Gets the page number, which should be either the first or last index of the array.
-            // Removing that index normalizes the array so future operations don't depend on which 
-            // orientation the array is in.
-            // In the instance that the page number is in the last index, the OCR software puts the
-            // name of the section and the page number on the same line, with the page number being last
-            if (intval($lines[0])) {
-                $page = array_shift($lines);
-            } else {
-                $page_arr = explode(' ', array_pop($lines));
-                $page = $page_arr[sizeof($page_arr) - 1];
-
-                if (!intval($page)) {
-                    $page = '';
-                }
-            }
-            foreach ($lines as $ln => $line) {
-                if (strpos($line, 'SERVES') !== false) {
-                    break;
-                }
-            }
-            $title = ucwords(strtolower(implode(' ', array_slice($lines, 0, $ln))));
+        public function save_recipe_parts_to_acf_fields($response, $post_id) {
             // Get the servings and total time
-            $split = explode('TOTAL TIME:', $lines[$ln]);
-            $servings = $split[0];
-            $time = $split[1];
-            wp_update_post([
-                'ID'            => $post_id,
-                'post_title'    => $title,
-                'post_content'  => $recipe,
-                'post_name'     => str_replace(' ', '-', strtolower($title))
-            ]);
-            
-            update_field('page', $page, $post_id);
-            update_field('servings', $servings, $post_id);
-            update_field('time', $time, $post_id);
+            foreach ($response as $index) {
+                switch ($index['key']) {
+                    case 'title':
+                        $title = ucwords(strtolower(implode(' ', $index['values'])));
+                        wp_update_post([
+                            'ID' => $post_id,
+                            'post_title' => $title,
+                            'post_name' => str_replace(' ', '-', strtolower($title))
+                        ]);
+                        break;
+                    case 'page number':
+                        update_field('page', $index['values'][0], $post_id);
+                        break;
+                    case 'servings':
+                        update_field('servings', strtolower($index['values'][0]), $post_id);
+                        break;
+                    case 'cook time':
+                        update_field('time', strtolower($index['values'][0]), $post_id);
+                        break;
+                    case 'ingredients':
+                        $ingredients = implode("\r\n", $index['values']);
+                        update_field('ingredients', $ingredients, $post_id);
+                        break;
+                    case 'recipe steps':
+                        $steps = implode("\r\n", $index['values']);
+                        update_field('cooking_instructions', $steps, $post_id);
+                        break;
+                    default:
+                        echo $index['key'];
+                        break;
+                }
+            }
         }
 
         public function filter_posts($query) {
